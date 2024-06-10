a72b2d447f6437c13dbf33b9465fc2a3c90c3554
Jomana ahmed <ojy.ahmed@outlook.com>
2024-06-10 15:36:49 +0200

    Creating Header.js file

---
+++ src/components/HomePage/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  background-color: #ffffe6;
+`
+
+export default Header;
