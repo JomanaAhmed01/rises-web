eb51fa6cbf4b21be886f98bb816de93be9320862
Jomana ahmed <ojy.ahmed@outlook.com>
2024-06-13 17:35:05 +0200

    Creating Journey.js file

---
+++ src/components/HomePage/Journey.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function Journey() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div``;
+
+export default Journey;
