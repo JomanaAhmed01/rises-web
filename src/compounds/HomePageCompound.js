aebe3d648c36a8ac9372ba46e147167405414a95
Jomana ahmed <ojy.ahmed@outlook.com>
2024-06-13 17:35:53 +0200

    Adding Journey.js file to the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/HomePage/Navbar";
 import Header from "../components/HomePage/Header";
+import Journey from "../components/HomePage/Journey";
 
 function HomePageCompound() {
   return (
     <Wrapper>
       <Navbar />
       <Header />
+      <Journey />
     </Wrapper>
   );
 }
