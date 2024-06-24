cd1ab9ca5ec7318db2f75e8d066708caa0e2f2a5
Jomana ahmed <ojy.ahmed@outlook.com>
2024-06-24 14:52:16 +0200

    Creating Industries.js file

---
+++ src/components/HomePage/Industries.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function Industries() {
+  return (
+    <Wrapper>
+      <MainWrapper></MainWrapper>
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div``;
+
+export const MainWrapper = styled.div``;
+
+export default Industries;
